75c7d1e76ad90a854cb74bf15bce5b1c64c5f3e1
Maximiliano Boggio <maxi-boggio.mdq@hotmail.com>
2021-01-27 09:24:19 -0300

    Create 12-ternario.js

---
+++ 02-intro-javascript/src/bases/12-ternario.js
@@ -0,0 +1,11 @@
+const activo = false;
+
+//OPERADOR TERNARIO
+const mensaje = (activo) ? 'Activo' : 'Inactivo';
+
+console.log(mensaje);
+
+//CON CONDICION AND PARA PODER HACER UN IF MAS RAPIDO
+const M1 = !activo && 'Activo';
+
+console.log(M1);
